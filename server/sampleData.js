aaf0db84574a6a54551cb20b9da79a32f3c1ff56
Jason Gong <mhpjay@gmail.com>
2023-09-12 13:21:23 -0400

    setup server, schema, base graphQL, data plus DB and created models

---
+++ server/sampleData.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'Project 1',
+    description: 'This is project 1',
+    status: 'active',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Project 2',
+    description: 'This is project 2',
+    status: 'inactive',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'Project 3',
+    description: 'This is project 3',
+    status: 'active',
+  },
+];
+
+const clients = [
+  {
+    id: '1',
+    name: 'Client 1',
+    email: 'client1@email.com'
+  },
+  {
+    id: '2',
+    name: 'Client 2',
+    email: 'client2@email.com'
+  },
+  {
+    id: '3',
+    name: 'Client 3',
+    email: 'client3@email.com'
+  },
+];
+
+module.exports = {
+  projects,
+  clients,
+};
